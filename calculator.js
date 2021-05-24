1b5ab48046c320802cacd1acfd4b81842eacad9f
Sai Alankrutha Pandra Pagada <alankrutha236@gmail.com>
2021-05-24 19:38:24 -0400

    further study

---
+++ calculator.js
@@ -0,0 +1,41 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+reader.question("What would you like to calculate?", function(input){
+	 var tokens = input.split(' ');
+	
+	var mathSymbol = tokens[0];
+	 var num1 = Number(tokens[1]);
+	 console.log('num1',num1)
+	 var num2 = Number(tokens[2]);
+	 console.log('num2',num2)
+	 if(mathSymbol === '+'){
+		 console.log(num1+num2)
+	 }
+	 if(mathSymbol === '*'){
+		console.log(num1+num2)
+	}
+	if(mathSymbol === '-'){
+		console.log(num1+num2)
+	}
+	if(mathSymbol === '/'){
+		console.log(num1+num2)
+	}
+  if (mathSymbol === "sqr"){
+        console.log(Math.pow(num1,2))
+    }
+   if (mathSymbol === "cbrt"){
+        console.log(Math.cbrt(num1,num2))
+    }
+    if (mathSymbol === "pow"){
+        console.log(Math.pow(num1,num2))
+    }
+    if (mathSymbol === "mod"){
+        console.log(num1 % num2)
+    }
+	// This line closes the connection to the command line interface.
+	reader.close()
+});
